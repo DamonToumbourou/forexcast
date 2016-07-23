1b4d4bc9f5405c1ca93c2ea112fb65dfa24fa012
DamonToumbourou <dtoumbourou@gmail.com>
2016-07-23 16:02:07 +1000

    building framework

--- forexcast.py
+++ forexcast.py
@@ -5,4 +5,21 @@ import requests
 app = Flask(__name__)
 
 @app.route('/')
-def 
+def greeting():
+    return render_template('greeting.html')
+
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/sentiment')
+def sentiment():
+    return render_template('sentiment.html')
+
+def get_sentiment():
+    return None
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
+
