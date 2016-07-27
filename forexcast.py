e88472da7ba1fdf4da9d3e575e4e3d4e13e65719
DamonToumbourou <dtoumbourou@gmail.com>
2016-07-27 12:24:06 +1000

    add daily fx sentiment

--- forexcast.py
+++ forexcast.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, g
 from bs4 import BeautifulSoup as bs
-import requests 
+from selenium import webdriver
+import requests
+import urlparse
 import re
 
 app = Flask(__name__)
@@ -14,28 +16,36 @@ def home():
 
 @app.route('/sentiment')
 def sentiment():
-    usd_jpy = get_ig_sentiment('usd-jpy')
-    eur_usd = get_ig_sentiment('eur-usd')
-    gbp_usd = get_ig_sentiment('gbp-usd')
-    aud_usd = get_ig_sentiment('aud-usd')
+    # IG Sentiment
+    ig_usd_jpy = get_ig_sentiment('usd-jpy')
+    ig_eur_usd = get_ig_sentiment('eur-usd')
+    ig_gbp_usd = get_ig_sentiment('gbp-usd')
+    ig_aud_usd = get_ig_sentiment('aud-usd')
 
-    usd_jpy_long = usd_jpy[0]['long']
-    usd_jpy_short = 100 - int(usd_jpy_long)
+    ig_usd_jpy_long = ig_usd_jpy[0]['long']
+    ig_usd_jpy_short = 100 - int(ig_usd_jpy_long)
 
-    eur_usd_long = eur_usd[0]['long']
-    eur_usd_short = 100 - int(eur_usd_long)
+    ig_eur_usd_long = ig_eur_usd[0]['long']
+    ig_eur_usd_short = 100 - int(ig_eur_usd_long)
 
-    gbp_usd_long = gbp_usd[0]['long']
-    gbp_usd_short = 100 - int(gbp_usd_long)
+    ig_gbp_usd_long = ig_gbp_usd[0]['long']
+    ig_gbp_usd_short = 100 - int(ig_gbp_usd_long)
     
-    aud_usd_long = aud_usd[0]['long']
-    aud_usd_short = 100 - int(aud_usd_long)
+    ig_aud_usd_long = ig_aud_usd[0]['long']
+    ig_aud_usd_short = 100 - int(ig_aud_usd_long)
+    
+    # DAILY FX Sentiment
+    results = get_dailyfx_sentiment()
+    dy_eur_usd_long = results[0]['dy_eur_usd_long']
+    dy_eur_usd_short = results[1]['dy_eur_usd_short']
 
     return render_template('sentiment.html', \
-            usd_jpy_long=usd_jpy_long, usd_jpy_short=usd_jpy_short, \
-            eur_usd_long=eur_usd_long, eur_usd_short=eur_usd_short, \
-            gbp_usd_long=gbp_usd_long, gbp_usd_short=gbp_usd_short, \
-            aud_usd_long=aud_usd_long, aud_usd_short=aud_usd_short) 
+            ig_usd_jpy_long=ig_usd_jpy_long, ig_usd_jpy_short=ig_usd_jpy_short, \
+            ig_eur_usd_long=ig_eur_usd_long, ig_eur_usd_short=ig_eur_usd_short, \
+            ig_gbp_usd_long=ig_gbp_usd_long, ig_gbp_usd_short=ig_gbp_usd_short, \
+            ig_aud_usd_long=ig_aud_usd_long, ig_aud_usd_short=ig_aud_usd_short, \
+            dy_eur_usd_long=dy_eur_usd_long, dy_eur_usd_short=dy_eur_usd_short) 
+
 
 def get_ig_sentiment(trading_pair):
     url = 'http://www.ig.com/au/ig-forex/' + trading_pair
@@ -48,6 +58,60 @@ def get_ig_sentiment(trading_pair):
     long_ = re.search('[0-9]*', long_).group(0)
 
     result.append({'long': long_})
+
+    return result
+
+
+def get_dailyfx_sentiment(): 
+    # Had to use PhantomJS to get Javascript on page
+    url = 'http://www.dailyfx.com/sentiment'
+    browser = webdriver.PhantomJS()
+    browser.get(url) 
+    html = browser.page_source
+    soup = bs(html, 'html.parser')
+    
+    # Store results
+    result =[]
+    
+    # Find results in html using bs4
+    sentiment = soup.find('div', {'class': 'sentiment-panel'})
+    sentiment = soup.find_all('div', {'class': 'col-xs-6'})
+
+    # EUR/USD
+    eur_usd = sentiment[1].find('span', {'class': 'bullish-color'})
+    eur_usd = eur_usd.get_text() 
+    dy_eur_usd_long = re.search('[0-9]*', eur_usd).group(0)
+    dy_eur_usd_short = 100 - int(dy_eur_usd_long)
+    
+    result.append({'dy_eur_usd_long': dy_eur_usd_long})
+    result.append({'dy_eur_usd_short': dy_eur_usd_short})
+    
+    # USD/JPY
+    usd_jpy = sentiment[2].find('span', {'class': 'bullishcolor'})
+    usd_jpy = usd_jpy.get_text()
+    dy_usd_jpy_long = re.search('[0-9]*', usd_jpy).group(0)
+    dy_usd_jpy_short = 100 - int(dy_usd_jpy_long)
+
+    result.append({'dy_usd_jpy_long': dy_usd_jpy_long})
+    result.append({'dy_usd_jpy_short': dy_usd_jpy_short})
+
+    # GBP/USD
+    gbp_usd = sentiment[3].find('span', {'class': 'bullishcolor'})
+    gbp_usd = usd_jpy.get_text()
+    dy_gbp_usd_long = re.search('[0-9]*', gbp_usd).group(0)
+    dy_gbp_usd_short = 100 - int(dy_gbp_usd_long)
+
+    result.append({'dy_gbp_usd_long': dy_gbp_usd_long})
+    result.append({'dy_gbp_usd_short': dy_gbp_usd_short})
+    
+    # AUD/USD
+    aud_usd = sentiment[4].find('span', {'class': 'bullishcolor'})
+    aud_usd = usd_jpy.get_text()
+    dy_aud_usd_long = re.search('[0-9]*', aud_usd).group(0)
+    dy_aud_usd_short = 100 - int(dy_aud_usd_long)
+
+    result.append({'dy_aud_usd_long': dy_aud_usd_long})
+    result.append({'dy_aud_usd_short': dy_aud_usd_short})
 
     return result
 
